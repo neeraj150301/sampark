26723d3b3c21961b4c34c80e3d68e7fba3f669a3
neeraj <neerajs150301@gmail.com>
2024-10-24 16:30:34 +0530

    theme in shared preference

--- lib/controller/theme_controller.dart
+++ lib/controller/theme_controller.dart
@@ -1,16 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeController extends GetxController {
   var isDarkMode = false.obs;
 
-  void toggleTheme() {
+
+  @override
+  void onInit() {
+    super.onInit();
+    _loadThemeFromPrefs(); // Load theme mode when the controller is initialized
+  }
+
+  void toggleTheme() async {
     if (Get.isDarkMode) {
       Get.changeThemeMode(ThemeMode.light);
       isDarkMode(false);
+      await _saveThemeToPrefs(false);
     } else {
       Get.changeThemeMode(ThemeMode.dark);
       isDarkMode(true);
+      await _saveThemeToPrefs(true);
     }
   }
+
+    // Save theme preference
+  Future<void> _saveThemeToPrefs(bool isDark) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('isDarkMode', isDark);
+  }
+  
+    // Load theme preference
+  Future<void> _loadThemeFromPrefs() async {
+    final prefs = await SharedPreferences.getInstance();
+    isDarkMode.value = prefs.getBool('isDarkMode') ?? false;
+    if (isDarkMode.value) {
+      Get.changeThemeMode(ThemeMode.dark);
+    } else {
+      Get.changeThemeMode(ThemeMode.light);
+    }
+  }
+  
 }
